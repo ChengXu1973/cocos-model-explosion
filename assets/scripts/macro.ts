9715231a9e551f9d0791fd3d7bc863da616861cf
xucheng <xucheng@mail.jj.cn>
2024-12-26 16:55:06 +0800

    [dev] create img

---
+++ assets/scripts/macro.ts
@@ -0,0 +1,2 @@
+export const TEX_WIDTH = 512;
+export const TEX_HEIGHT = 512;
--- assets/scripts/main.ts
+++ assets/scripts/main.ts
@@ -1,21 +1,53 @@
-import { _decorator, Component, MeshRenderer, RenderTexture } from 'cc';
+import { _decorator, clamp, Component, gfx, ImageAsset, MeshRenderer, Sprite, SpriteFrame, Texture2D } from 'cc';
+import { TEX_HEIGHT, TEX_WIDTH } from './macro';
 const { ccclass, property } = _decorator;
 
 @ccclass('Main')
 export class Main extends Component {
-    @property(RenderTexture)
-    rt: RenderTexture = null;
-
     @property(MeshRenderer)
     model: MeshRenderer = null;
 
+    @property(Sprite)
+    sprite: Sprite = null;
+
     onLoad() {
-        if (this.rt && this.model) {
+        if (this.model) {
             this._bake();
         }
     }
 
     private _bake() {
-        
+        const mesh = this.model.mesh;
+        mesh.renderingSubMeshes[0].enableVertexIdChannel(gfx.deviceManager.gfxDevice);
+
+        const positions = mesh.readAttribute(0, gfx.AttributeName.ATTR_POSITION) as Float32Array;
+        const indices = mesh.readIndices(0) as Uint32Array;
+        const max = mesh.struct.maxPosition.clone();
+        const min = mesh.struct.minPosition.clone();
+        const range = max.clone().subtract(min);
+
+        const width = TEX_WIDTH;
+        const height = TEX_HEIGHT;
+        const _data = new Float32Array(width * height * 4);
+        const _compressed = false;
+        const format = Texture2D.PixelFormat.RGBA32F;
+
+        for (let i = 0; i < indices.length; i++) {
+            const x = positions[i * 3 + 0];
+            const y = positions[i * 3 + 1];
+            const z = positions[i * 3 + 2];
+            _data[i * 4 + 0] = clamp((x - min.x) / range.x, 0, 1);
+            _data[i * 4 + 1] = clamp((y - min.y) / range.y, 0, 1);
+            _data[i * 4 + 2] = clamp((z - min.z) / range.z, 0, 1);
+            _data[i * 4 + 3] = 1;
+        }
+
+        const img = new ImageAsset({ _data, width, height, format, _compressed });
+        const tex = new Texture2D();
+        tex.image = img;
+        this.sprite.spriteFrame = new SpriteFrame();
+        this.sprite.spriteFrame.texture = tex;
+
+        this.model.getSharedMaterial(0).setProperty("modelTexture", tex);
     }
 }
