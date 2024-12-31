55168659d215e8131bac55e6d85f7f1f780c42c1
xucheng <xucheng@mail.jj.cn>
2024-12-31 11:04:58 +0800

    [dev] indices

--- assets/scripts/macro.ts
+++
@@ -1,2 +0,0 @@
-export const TEX_WIDTH = 9;
-export const TEX_HEIGHT = 9;
--- assets/scripts/main.ts
+++ assets/scripts/main.ts
@@ -1,8 +1,9 @@
-import { _decorator, clamp, Component, gfx, ImageAsset, MeshRenderer, Sprite, SpriteFrame, Texture2D } from 'cc';
-import { TEX_HEIGHT, TEX_WIDTH } from './macro';
+import { _decorator, clamp, Component, EPSILON, gfx, ImageAsset, math, MeshRenderer, Sprite, SpriteFrame, Texture2D, utils, v4 } from "cc";
 const { ccclass, property } = _decorator;
 
-@ccclass('Main')
+const animParam = v4();
+
+@ccclass("Main")
 export class Main extends Component {
     @property(MeshRenderer)
     model: MeshRenderer = null;
@@ -11,29 +12,76 @@ export class Main extends Component {
     sprite: Sprite = null;
 
     onLoad() {
-        if (this.model) {
-            this._bake();
+        if (!this.model) {
+            return;
         }
+        this._bake();
     }
 
     private _bake() {
-        const mesh = this.model.mesh;
-        mesh.renderingSubMeshes[0].enableVertexIdChannel(gfx.deviceManager.gfxDevice);
-
-        const positions = mesh.readAttribute(0, gfx.AttributeName.ATTR_POSITION) as Float32Array;
-        const count = ~~(positions.length / 3);
-
-        const max = mesh.struct.maxPosition.clone();
-        const min = mesh.struct.minPosition.clone();
+        // aabb
+        const max = this.model.mesh.struct.maxPosition.clone();
+        const min = this.model.mesh.struct.minPosition.clone();
         const range = max.clone().subtract(min);
+        range.x = Math.max(range.x, EPSILON);
+        range.y = Math.max(range.y, EPSILON);
+        range.z = Math.max(range.z, EPSILON);
+        this.model.getSharedMaterial(0).setProperty("minPos", v4(min.x, min.y, min.z));
+        this.model.getSharedMaterial(0).setProperty("modelSize", v4(range.x, range.y, range.z));
+        // bake
+        const positions = this._reCreateMesh();
+        const posTex = this._bakeVertexTexture(positions, min, range);
+        this.model.getSharedMaterial(0).setProperty("vertexTexture", posTex);
+        this.model.getSharedMaterial(0).setProperty("textureSize", v4(posTex.width, posTex.height));
+        // gl_VertexIndex
+        this.model.mesh.renderingSubMeshes[0].enableVertexIdChannel(
+            gfx.deviceManager.gfxDevice
+        );
+        // debug
+        if (!this.sprite) {
+            return;
+        }
+        this.sprite.spriteFrame = new SpriteFrame();
+        this.sprite.spriteFrame.texture = posTex;
+    }
 
-        const width = TEX_WIDTH;
-        const height = TEX_HEIGHT;
-        const _data = new Float32Array(width * height * 4);
+    private _reCreateMesh() {
+        const attrPositions = this.model.mesh.readAttribute(0, gfx.AttributeName.ATTR_POSITION) as Float32Array;
+        const attrNormals = this.model.mesh.readAttribute(0, gfx.AttributeName.ATTR_NORMAL) as Float32Array;
+        const attrTexCoord = this.model.mesh.readAttribute(0, gfx.AttributeName.ATTR_TEX_COORD) as Float32Array;
+        const indices = this.model.mesh.readIndices(0) as Uint32Array;
+        // recreate mesh
+        const count = indices.length;
+        const positions = Array<number>(count * 3).fill(0);
+        const normals = Array<number>(count * 3).fill(0);
+        const uvs = Array<number>(count * 2).fill(0);
+        indices.forEach((index, count) => {
+            // pos
+            positions[count * 3 + 0] = attrPositions[index * 3 + 0];
+            positions[count * 3 + 1] = attrPositions[index * 3 + 1];
+            positions[count * 3 + 2] = attrPositions[index * 3 + 2];
+            // normal
+            normals[count * 3 + 0] = attrNormals[index * 3 + 0];
+            normals[count * 3 + 1] = attrNormals[index * 3 + 1];
+            normals[count * 3 + 2] = attrNormals[index * 3 + 2];
+            // uv
+            uvs[count * 2 + 0] = attrTexCoord[index * 2 + 0];
+            uvs[count * 2 + 1] = attrTexCoord[index * 2 + 1];
+        });
+        this.model.mesh = utils.MeshUtils.createMesh({ positions, normals, uvs });
+        this.model.onGeometryChanged();
+        return positions;
+    }
+
+    private _bakeVertexTexture(positions: number[], min: math.Vec3, range: math.Vec3) {
+        const len = positions.length / 3;
+        const offset = Math.ceil(Math.log2(Math.sqrt(len)));
+        const width = 1 << offset;
+        const height = 1 << offset;
         const _compressed = false;
         const format = Texture2D.PixelFormat.RGBA32F;
-
-        for (let i = 0; i < count; i++) {
+        const _data = new Float32Array(width * height * 4);
+        for (let i = 0; i < len; i++) {
             const x = positions[i * 3 + 0];
             const y = positions[i * 3 + 1];
             const z = positions[i * 3 + 2];
@@ -42,16 +90,14 @@ export class Main extends Component {
             _data[i * 4 + 2] = clamp((z - min.z) / range.z, 0, 1);
             _data[i * 4 + 3] = 1;
         }
-
-        const img = new ImageAsset({ _data, width, height, format, _compressed });
         const tex = new Texture2D();
         tex.setFilters(Texture2D.Filter.NONE, Texture2D.Filter.NONE);
-        tex.image = img;
-        this.sprite.spriteFrame = new SpriteFrame();
-        this.sprite.spriteFrame.texture = tex;
+        tex.image = new ImageAsset({ _data, width, height, format, _compressed });
+        return tex;
+    }
 
-        this.model.getSharedMaterial(0).setProperty("modelTexture", tex);
-        this.model.getSharedMaterial(0).setProperty("size", range);
-        this.model.getSharedMaterial(0).setProperty("min", min);
+    private _setAnim() {
+        const pass0 = this.model.getSharedMaterial(0).passes[0];
+        pass0.setUniform(pass0.getHandle("animParam"), animParam);
     }
 }
